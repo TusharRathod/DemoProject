ef0b63be8b870a0093d720979be2d7edb07a8ac4
tushar rathod <tusharrathore353@gmail.com>
2020-03-06 12:15:17 +0530

    Product Data file for store in database

---
+++ data.js
@@ -0,0 +1,145 @@
+[
+  {
+    id: "1",
+    imgUrl:
+      "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    name: "blue hiking backpack ",
+    price: 2449,
+    rating: 5,
+    review: 57,
+    favorite: true,
+    actualPrice: 5000,
+    discountPercent: 51
+  },
+  {
+    id: "2",
+    imgUrl:
+      "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+    name: "CHECK PRINT SHIRT",
+    price: 2099,
+    rating: 4,
+    review: 90,
+    favorite: true,
+    actualPrice: 4400,
+    discountPercent: 52
+  },
+  {
+    id: "3",
+    imgUrl:
+      "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+    name: "GLORIA HIGH LOGO SNEAKER",
+    price: 1894,
+    rating: 4.4,
+    review: 390,
+    favorite: true,
+    actualPrice: 3300,
+    discountPercent: 42
+  },
+  {
+    id: "4",
+    imgUrl:
+      "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+    name: "CATE RIGID BAG",
+    price: 2499,
+    rating: 3.4,
+    review: 210,
+    favorite: true,
+    actualPrice: 5600,
+    discountPercent: 55
+  },
+  {
+    id: "5",
+    imgUrl:
+      "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
+    name: "GUESS CONNECT WATCH",
+    price: 2099,
+    rating: 4.5,
+    review: 560,
+    favorite: true,
+    actualPrice: 4000,
+    discountPercent: 47
+  },
+  {
+    id: "6",
+    imgUrl:
+      "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
+    name: "'70s RETRO GLAM KEFIAH",
+    price: 1499,
+    rating: 5,
+    review: 900,
+    favorite: true,
+    actualPrice: 3999,
+    discountPercent: 62
+  },
+  {
+    id: "7",
+    imgUrl:
+      "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-superJumbo.jpg?quality=90&auto=webp",
+    name: "DISAPPERING EARTH ",
+    price: 1449,
+    rating: 4.5,
+    review: 103,
+    favorite: true,
+    actualPrice: 2000,
+    discountPercent: 44
+  },
+  {
+    id: "8",
+    imgUrl:
+      "https://thoughtcatalog.files.wordpress.com/2013/11/manssearchformeaning.jpg?w=1920&h=1280&crop=1&resize=1920,1280&quality=95&strip=all",
+    name: "MAN'S SEARCH  FOR MEANING ",
+    price: 449,
+    rating: 3.2,
+    review: 57,
+    favorite: true,
+    actualPrice: 1000,
+    discountPercent: 52
+  },
+  {
+    id: "9",
+    imgUrl: "https://images-na.ssl-images-amazon.com/images/I/41xCWDx-OyL.jpg",
+    name: "MAN'S BLUE CORTON SHIRT WITH FULL SLEEVS ",
+    price: 2449,
+    rating: 4.1,
+    review: 89,
+    favorite: true,
+    actualPrice: 4000,
+    discountPercent: 56
+  },
+  {
+    id: "10",
+    imgUrl:
+      "https://5.imimg.com/data5/YM/SL/MY-29418413/rose-rolex-watch-500x500.jpg",
+    name: "ROSE GOLDEN ROLEX WATCH (MADE IN FRACH) ",
+    price: 200000,
+    rating: 4.3,
+    review: 900,
+    favorite: true,
+    actualPrice: 450000,
+    discountPercent: 60
+  },
+  {
+    id: "11",
+    imgUrl:
+      "https://4.imimg.com/data4/EW/YU/MY-11616214/diamond-ring-500x500.jpg",
+    name: "QUEEN'S DIAMOND RING ",
+    price: 340000,
+    rating: 1.8,
+    review: 80,
+    favorite: true,
+    actualPrice: 450000,
+    discountPercent: 43
+  },
+  {
+    id: "12",
+    imgUrl:
+      "https://www.orra.co.in/pub/media/catalog/product/o/s/osn17032-a.png",
+    name: "QUEEN'S DIAMOND AND PLATINUM NECKLACE ",
+    price: 5000000,
+    rating: 4.2,
+    review: 1080,
+    favorite: true,
+    actualPrice: 5000000,
+    discountPercent: 0
+  }
+];
